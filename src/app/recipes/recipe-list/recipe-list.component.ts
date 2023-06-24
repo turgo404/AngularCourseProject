4e0297d3b79dfdafc8632cb2780a61c9c226a771
Turgay Orhanov <Turgod@abv.bg>
2023-06-24 18:07:31 +0300

    Recipes_component_functionality_added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,17 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 import {Recipe} from '../recipe.model';
-
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+@Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     //ALTERNATIVE IMAGE PATH    ----------   https://ichef.bbci.co.uk/food/ic/food_16x9_1600/recipes/one_pan_sausage_pasta_55251_16x9.jpg
     new Recipe('A Test Recipe', 'This is simply a test', `https://encrypted-tbn0.gstatic.com/
+    images?q=tbn:ANd9GcSvUAtV4MGrlgRCAUqB7X3ARtN-Z9rdZDoxiA&usqp=CAU`),
+    new Recipe('Another Test Recipe', 'This is simply a test', `https://encrypted-tbn0.gstatic.com/
     images?q=tbn:ANd9GcSvUAtV4MGrlgRCAUqB7X3ARtN-Z9rdZDoxiA&usqp=CAU`)
   ];
 
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
+
 }
+
+
